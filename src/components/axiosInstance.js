36d2507ba969c1609a0f0cdc62d27965bd3105fd
Ahmed Fathy <a.ramadan.ext@tahaluf.ai>
2024-05-29 11:53:03 +0300

    fix api

--- src/components/axiosInstance.js
+++ src/components/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://www.krokplus.com/api/v1',
+    baseURL: 'https://app.krokplus.com/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
